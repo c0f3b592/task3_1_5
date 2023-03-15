039699f7f36d98e869bf8fb63639ebfc824b6880
c0f3b592 <c0f3b592@gmail.com>
2023-03-15 23:28:46 +0300

    3_1_5 fix

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -2,17 +2,9 @@ package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import ru.kata.spring.boot_security.demo.service.RoleService;
 
 @Controller
 public class AdminController {
-
-    private final RoleService roleService;
-
-    public AdminController(RoleService roleService) {
-        this.roleService = roleService;
-    }
-
     @GetMapping(value = "/admin")
     public String printUsers() {
         return "admin";
